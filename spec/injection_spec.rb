3afbcada7877366fc76cb803aaeff63c8622d774
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-02-23 12:00:12 +0000

    Add tests finished

--- spec/injection_spec.rb
+++ spec/injection_spec.rb
@@ -1,5 +1,9 @@
 describe Note do
   describe "#display" do
     it "Prints a title with a body" do
-
+      formatter_class = NoteFormatter.new
+      note = Note.new('title', 'body', formatter_class)
+      expect(note.display).to eq "Title: #{note.title}\n#{note.body}"
     end
+  end
+end
--- spec/note_formater_spec.rb
+++ spec/note_formater_spec.rb
@@ -1,9 +1,10 @@
+
 describe Note do
   describe "#display" do
-    it "Prints a title with a body" do
-      formatter_class = NoteFormatter.new
-      note = Note.new('title', 'body', formatter_class)
-      expect(note.display).to eq "Title: #{note.title}\n#{note.body}"
+    it "formats the display" do
+      formatter_class =  double(:format =>  "Title: tilte\nbody")
+      note = Note.new('title', 'body', note_formatter)
+      enxpect(note.display).to eq "Title: #{note.title}\n#{note.body}"
     end
   end
 end
